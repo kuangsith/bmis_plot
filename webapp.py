458f08bc5ea3d89d6ff0f3791c6d78c91ae6f0e8
Sittipong T <108988177+kuangsith@users.noreply.github.com>
2024-02-21 22:29:03 +0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,13 +9,17 @@ st.header('BMIS DATA')
 
 
 with st.expander("Setting"):
-    st.write(f"Will add things here")
+    st.write(f"Table name and column name will be selected here")
 
 js = datalib.get_since_long('2024-02-15','2024-02-18')
 datalib.process_json(js)
 df = datalib.processed_json_to_df(js)
 
-st.line_chart(data=df,x='time',y='WaterTemp_ACTW')
+
+collist= [col for col in df.columns if col not in ['time','no']]
+
+for col in collist:
+    st.line_chart(data=df,x='time',y=col)
 
 
 
