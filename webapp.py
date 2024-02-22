e9653c2675f27291fd69ba2a70baf9b2a5491180
Sittipong T <108988177+kuangsith@users.noreply.github.com>
2024-02-22 20:17:18 +0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,6 +10,7 @@ import time
 st.header('BIMS DATA')
 
 npoints = 2400
+samplerate = 4
 
 
 with st.expander("Setting"):
@@ -40,9 +41,11 @@ collist= [col for col in df.columns if col not in ['TimeStamp','Record']]
 
 df['TimeStamp'] = pd.to_datetime(df['TimeStamp'])
 
+df_lite = df[(df.index % samplerate) == 0].copy().reset_index(drop=True)
+
 for col in collist:
     # print(col)
-    st.line_chart(data=df,x='TimeStamp',y=col,height=400)
+    st.line_chart(data=df_lite,x='TimeStamp',y=col,height=400)
 
 end = time.time()
 
