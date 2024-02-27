f8eb1fe5582656c3c2945335677c77b98aca2a35
Sittipong T <108988177+kuangsith@users.noreply.github.com>
2024-02-27 21:26:03 +0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -57,7 +57,8 @@ for col in collist:
     title=col,
     x_axis_label='Time',
     y_axis_label= unit_dict.get(col) if unit_dict.get(col) is not None else col,
-    x_axis_type="datetime")
+    x_axis_type="datetime",
+    width=300, height=200)
 
     p.line(source = df_lite, x= 'TimeStamp', y= col, line_width=2)
 
