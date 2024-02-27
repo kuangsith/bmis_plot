ec4678048936283858b19d627fc136bb4a983f54
Sittipong T <108988177+kuangsith@users.noreply.github.com>
2024-02-27 21:04:29 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+def tablist():
+    return ['ACTW','ACLW','AROW']
+
+def unit_dict():
+    return {'WaterTemp_ACTW':'°C',
+ 'Conductivity':None,
+ 'Salinity':'psu',
+ 'ACTWVoltage':None,
+ 'WaterTemp_ACLW':'°C',
+ 'Chlorophyll':'mg/m3',
+ 'Turbidity':None,
+ 'ACLWVoltage':None,
+ 'LoggerVoltage_Min':None,
+ 'LoggerTemp':'°C',
+ 'WaterTemp_AROW':'°C',
+ 'DO_AROW':'%',
+ 'AROWVoltage':None,
+ 'DO_mgL':'mg/L'
+ }
+
